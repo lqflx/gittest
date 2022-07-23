bda270ce2f7b2f8a352f97ab7abf1cec8b9c194e
lqf <515739360@qq.com>
2022-07-23 23:44:59 +0800

    Revert "sit 第二次提交11"
                                This reverts commit c4176d1b

--- src/main/java/com/lx/git/App.java
+++ src/main/java/com/lx/git/App.java
@@ -3,6 +3,6 @@ package com.lx.git;
 public class App {
 
     public static void main(String[] args) {
-
+        int i = 1/2;
     }
 }
--- src/main/java/com/lx/git/B.java
+++
@@ -1,8 +0,0 @@
-package com.lx.git;
-
-public class B {
-
-    public static void main(String[] args) {
-        System.out.println(1);
-    }
-}
