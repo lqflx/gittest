4cf7a4168dcf38f38d03d8223658a7cf13c57720
lqf <515739360@qq.com>
2022-07-23 19:35:10 +0800

    sit 第二次提交

--- src/main/java/com/lx/git/App.java
+++ src/main/java/com/lx/git/App.java
@@ -4,8 +4,5 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("111111111111");
-        System.out.println("测试一下");
-        //xin
-        System.out.println("1");
     }
 }
